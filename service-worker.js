f67f5832784e2c7e5dba7987ed3ff0d0a912ccd7
sohaibchekr <sohaibchekri@gmail.com>
2026-03-14 19:19:51 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gest-labo-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './logo.PNG'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch (Offline support)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
